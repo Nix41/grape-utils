06a5ad23f910744aa8ddc50576f996e5b6a53126
Nix41 <acprieto41@gmail.com>
2023-11-30 01:44:29 +0100

    Set up first test

---
+++ spec/grape/utils/templates/create_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require "record_class"
+
+RSpec.describe Grape::Utils::Templates::Create, type: :request do
+  subject { Class.new(Grape::API) }
+
+  def app
+    subject
+  end
+
+  describe "test api" do
+    context "mounting an API" do
+      let(:params) { { name: "Foo" } }
+
+      before do
+        subject.version "v1", using: :path
+        subject.format :json
+        subject.content_type :json, "application/json"
+
+        subject.namespace :cool do
+          mount Grape::Utils::Templates::Create, with: {
+            model: RecordClass.new,
+            entity: MyEntity,
+            required_params: %i[name]
+          }
+        end
+      end
+
+      it "mounts a post endpoint" do
+        post "/v1/cool/", params
+        expect(last_response.body).to eq([params].to_json)
+      end
+    end
+  end
+end
